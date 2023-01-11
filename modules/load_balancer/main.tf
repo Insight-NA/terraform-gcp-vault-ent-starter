30f58d442b8190ecb5d3047573afcef53fe4d856
Jesse Wilker <Jesse.Wilker@insight.com>
2023-01-11 10:52:20 -0500

    adding ext firewall

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -61,3 +61,67 @@ resource "google_compute_forwarding_rule" "lb" {
   subnetwork            = var.subnetwork
   target                = google_compute_region_target_https_proxy.lb.self_link
 }
+
+
+#External LB
+resource "google_compute_address" "external" {
+  name         = "${var.resource_name_prefix}-vault-external-lb"
+  address_type = "EXTERNAL"
+  network_tier = "STANDARD"
+  region       = "us-east1"
+  purpose      = "External access to Vault"
+}
+
+resource "google_compute_region_health_check" "lb-ext" {
+  name = "${var.resource_name_prefix}-vault-external-lb"
+
+  check_interval_sec = 30
+  description        = "The health check of the external load balancer for Vault"
+  timeout_sec        = 4
+
+  https_health_check {
+    port         = 8200
+    request_path = var.vault_lb_health_check
+  }
+}
+
+resource "google_compute_region_backend_service" "lb-ext" {
+  health_checks = [google_compute_region_health_check.lb-ext.self_link]
+  name          = "${var.resource_name_prefix}-vault-external-lb"
+  description           = "The backend service of the external load balancer for Vault"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_name             = "https"
+  protocol              = "HTTPS"
+  timeout_sec           = 10
+
+  backend {
+    group = var.instance_group
+    balancing_mode  = "UTILIZATION"
+    description     = "The instance group of the compute deployment for Vault"
+    capacity_scaler = 1.0
+  }
+}
+
+resource "google_compute_region_url_map" "lb-ext" {
+  default_service = google_compute_region_backend_service.lb-ext.self_link
+  name            = "${var.resource_name_prefix}-vault-external-lb"
+
+  description = "The URL map of the external load balancer for Vault"
+}
+
+resource "google_compute_region_target_https_proxy" "lb-ext" {
+  name             = "${var.resource_name_prefix}-vault-external-lb"
+  ssl_certificates = [var.ssl_certificate_name]
+  url_map          = google_compute_region_url_map.lb-ext.self_link
+
+  description = "The target HTTPS proxy of the external load balancer for Vault"
+}
+resource "google_compute_forwarding_rule" "lb-ext" {
+  name                  = "${var.resource_name_prefix}-vault-external-lb"
+  ip_address            = google_compute_address.external.address
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = 443
+  subnetwork            = var.subnetwork
+  target                = google_compute_region_target_https_proxy.lb-ext.self_link
+}
